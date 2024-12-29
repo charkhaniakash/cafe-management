d2c9f292c5ef2970dc5b959b5983b6b15dea7077
charkhaniakash <charkhaniakash@gmail.com>
2024-12-30 01:57:32 +0530

    bug fixed for the server file

--- server/server.js
+++ server/server.js
@@ -3,3 +3,4 @@ const http = require('http');
 const app = require('./index');
 const server = http.createServer(app);
 server.listen(process.env.PORT)
+console.log('Server is running on port', process.env.PORT);
